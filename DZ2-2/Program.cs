0a6ff5477756dd6aefe5fa040fc56a9b5a9674ac
Elena <Shherbakove@yandex.ru>
2023-02-18 18:12:04 +0300

    D

---
+++ DZ2-2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите пргограмму, которая выводит третью цифру заданного числа или показывает, что третьей цифры нет
+// 645-> 5
+// 78-> третьей цифры нет
+// 32679-> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 )
+{
+    Console.WriteLine("Число не трехзначное");
+}
+else 
+{
+    while (number >999)
+    {
+    number/=10;
+    }
+    System.Console.WriteLine($"{(number % 10)}");
+}
